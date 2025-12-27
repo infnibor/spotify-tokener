a62becee4cc021b07bc92161d66e8a026e70f754
infnibor <156368035+infnibor@users.noreply.github.com>
2025-12-27 13:56:19 +0100

    Reorder imports in query.go for clarity
    Moved import block to the top of the file and removed the duplicate import section to improve code organization and readability.

--- internal/query.go
+++ internal/query.go
@@ -1,29 +1,29 @@
 
 package internal
 
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"os"
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+	"strconv"
+	"log"
+
+	"github.com/chromedp/chromedp"
+	"github.com/chromedp/cdproto/network"
+	"github.com/chromedp/cdproto/fetch"
+)
+
 const (
 	errEmptyURI           = "empty uri"
 	prefixTrack           = "spotify:track:"
 	prefixAlbum           = "spotify:album:"
 	prefixPlaylist        = "spotify:playlist:"
 	pathfinderQuery       = "pathfinder/v2/query"
-)
-
-import (
-       "context"
-       "encoding/json"
-       "errors"
-       "os"
-       "net/http"
-       "strings"
-       "sync"
-       "time"
-       "strconv"
-       "log"
-
-       "github.com/chromedp/chromedp"
-       "github.com/chromedp/cdproto/network"
-       "github.com/chromedp/cdproto/fetch"
 )
 
 var (
